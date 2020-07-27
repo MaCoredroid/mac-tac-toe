dc910eb709d6805dc0df938afb6d9418d8f79e10
macoredroid <coredroid0401@gmail.com>
2020-07-27 20:36:49 +0800

    first commit

---
+++ players/random.py
@@ -0,0 +1,39 @@
+import random
+
+from engine import MainBoardCoords, SubBoardCoords, SubBoard
+from players.stdout import StdOutPlayer
+
+
+class Random(StdOutPlayer):
+    def __init__(self):
+        super().__init__()
+
+    def get_my_move(self):  # -> Tuple[MainBoardCoords, SubBoardCoords]
+        main_board_coords = self.pick_next_main_board_coords()
+        sub_board = self.main_board.get_sub_board(main_board_coords)
+        sub_board_coords = self.pick_random_sub_board_coords(sub_board)
+        return main_board_coords, sub_board_coords
+
+    def pick_next_main_board_coords(self) -> MainBoardCoords:
+        if self.main_board.sub_board_next_player_must_play is None:
+            return random.choice(self.main_board.get_playable_coords())
+        else:
+            return self.main_board.sub_board_next_player_must_play
+
+    @staticmethod
+    def pick_random_sub_board_coords(sub_board: SubBoard) -> SubBoardCoords:
+        for sub_board_coords in sub_board.get_playable_coords():
+            copy_board=sub_board
+            copy_board=copy_board.add_my_move(sub_board_coords)
+            if copy_board.is_finished:
+                return sub_board_coords          
+        return random.choice(sub_board.get_playable_coords())
+
+    def timeout(self):
+        return
+
+    def game_over(self, winLoseTie: str, main_board_coords: MainBoardCoords, sub_board_coords: SubBoardCoords):
+        return
+
+    def match_over(self, winLoseTie: str, main_board_coords: MainBoardCoords, sub_board_coords: SubBoardCoords):
+        return
